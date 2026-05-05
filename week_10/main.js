441d7ceb1cb79f5f42587ffedc5c32c28e29ac6e
BamgasiJM <ryoong1125@gmail.com>
2026-05-05 21:15:53 +0900

    vpa class

--- week_10/main.js
+++ week_10/main.js
@@ -99,7 +99,7 @@ function displayCode(type) {
 
     const iframe = document.createElement("iframe");
     iframe.src =
-      "https://drive.google.com/file/d/1Sm-lF038MyIUDGIXYPKIPc4oFGIfib6S/preview";
+      "https://drive.google.com/file/d/1hFTVe42gGT1LIca34Y7pWFSldYAlnfJ6/preview";
     iframe.style.width = "100%";
     iframe.style.height = "100%";
     iframe.style.border = "none";
